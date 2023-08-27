3d56df62b95071b634395a7bb887722e58824411
kiat hee <cheekiat.hee@gmail.com>
2023-08-28 04:36:08 +0800

    aS

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
             /// Home
             SalomonBottomBarItem(
               icon: Icon(Icons.home),
-              title: Text("Home"),
+              title: Text("Homexxxxcxczcd"),
               selectedColor: Colors.purple,
             ),
 
